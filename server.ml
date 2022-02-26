f451b91aa43904b0cd7a5a22b80866e5a56a898e
Samuel Gallay <samuel.gallay@gmail.com>
2022-02-26 23:03:10 +0100

    Add a link to a PDF

--- server.ml
+++ server.ml
@@ -1,9 +1,17 @@
+let public_folder_url =
+  "https://storage.googleapis.com/erudite-descent-342509-public-bucket/documents/"
+
+let link filename text =
+  "<a href=\"" ^ public_folder_url ^ filename ^ "\">" ^ text ^ "</a>"
+
 let () =
   Dream.run ~interface:"0.0.0.0" ~port:8080
   @@ Dream.logger
   @@ Dream.router
        [
          Dream.get "/" (fun _ ->
-             Dream.html "This was deployed automatically (3) !");
+             Dream.html
+               ("This was deployed automatically (3) ! "
+               ^ link "rapport.pdf" "Rapport"));
          Dream.get "/static/**" @@ Dream.static "static";
        ]
