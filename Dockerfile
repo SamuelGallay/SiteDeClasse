18cb778f64df21dd4b812ab5b9e90a6c43933e2c
Samuel Gallay <samuel.gallay@gmail.com>
2022-02-27 16:14:51 +0100

    Document List

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM docker.io/ocaml/opam:debian-11-ocaml-4.12
-RUN ["/bin/bash", "-c", "sudo apt-get install -y libev-dev pkg-config libssl-dev screen"]
-RUN ["/bin/bash", "-c", "opam install -y dream"]
+RUN ["/bin/bash", "-c", "sudo apt-get install -y libev-dev pkg-config libssl-dev screen libgmp-dev"]
+RUN ["/bin/bash", "-c", "opam install -y dream ppx_yojson_conv cohttp-lwt-unix ppx_deriving"]
 COPY --chown=opam . SiteDeClasse
 WORKDIR SiteDeClasse
 RUN ["/bin/bash", "-c", "eval $(opam env) && dune build"]
--- server.ml
+++ server.ml
@@ -1,8 +1,43 @@
+open Cohttp
+open Cohttp_lwt_unix
+
+let ( let* ) = Lwt.bind
+let sprintf = Format.sprintf
+
 let public_folder_url =
   "https://storage.googleapis.com/erudite-descent-342509-public-bucket/documents/"
 
-let link filename text =
-  "<a href=\"" ^ public_folder_url ^ filename ^ "\">" ^ text ^ "</a>"
+let api =
+  "https://storage.googleapis.com/storage/v1/b/erudite-descent-342509-public-bucket/o"
+
+let link url text = "<a href=\"" ^ url ^ "\">" ^ text ^ "</a>"
+
+type storage_object = { name : string; mediaLink : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type object_list = { kind : string; items : storage_object list }
+[@@deriving yojson, show]
+
+let test_handler _r =
+  let* resp, body = Client.get (Uri.of_string api) in
+  let code = Code.code_of_status (Response.status resp) in
+  Dream.log "Object list %d" code;
+  (* let headers = Header.to_string (Response.headers resp) in *)
+  (* Dream.log "Headers %s" headers; *)
+  let* json_string = Cohttp_lwt.Body.to_string body in
+  (* Dream.log "Body of length: %d\n" (String.length body); *)
+  let json_safe = Yojson.Safe.from_string json_string in
+  (* Dream.log "Parsed to %a" Yojson.Safe.pp json_safe; *)
+  let obj_list = object_list_of_yojson json_safe in
+  Dream.log "\n%s" (show_object_list obj_list);
+  let s =
+    List.map
+      (fun obj ->
+        sprintf "<li><a href='%s'> %s </a></li>" obj.mediaLink obj.name)
+      obj_list.items
+    |> String.concat "\n\n"
+  in
+  Dream.html (sprintf "<ul>%s</ul>" s)
 
 let () =
   Dream.run ~interface:"0.0.0.0" ~port:8080
@@ -11,7 +46,8 @@ let () =
        [
          Dream.get "/" (fun _ ->
              Dream.html
-               ("This was deployed automatically (3) ! "
-               ^ link "rapport.pdf" "Rapport"));
+               ("This was deployed automatically (3) ! <br> "
+              ^ link "/test" "Documents"));
+         Dream.get "/test" test_handler;
          Dream.get "/static/**" @@ Dream.static "static";
        ]
